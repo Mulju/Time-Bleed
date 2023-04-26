ba7679ec57ad0ff9265ed66594b87cabe1745ec0
Haltimo Samuli <samuli.haltimo@metropolia.fi>
2023-04-26 13:38:47 +0300

    read data from mixer to slider

--- Assets/Scripts/MenuControl/AudioVolumeControl.cs
+++ Assets/Scripts/MenuControl/AudioVolumeControl.cs
@@ -2,10 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AudioVolumeControl : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] private Slider[] audioSliders;
+    [SerializeField] private bool updateSliders;
+
+    private void Awake()
+    {
+        if(updateSliders && SceneManager.GetActiveScene().name == "TimeBleed")
+        {
+            float masterVal, musicVal, soundVal;
+            audioMixer.GetFloat("masterVol", out masterVal);
+            audioMixer.GetFloat("musicVol", out musicVal);
+            audioMixer.GetFloat("soundVol", out soundVal);
+
+            audioSliders[0].value = Mathf.Pow(10, masterVal / 20);
+            audioSliders[1].value = Mathf.Pow(10, musicVal / 20);
+            audioSliders[2].value = Mathf.Pow(10, soundVal / 20);
+        }
+    }
 
     public void SetMasterVolume(float value)
     {
