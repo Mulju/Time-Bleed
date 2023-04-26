222f3eaa9a0929238298696d417ed18fc81caf65
Haltimo Samuli <samuli.haltimo@metropolia.fi>
2023-04-26 12:08:45 +0300

    Different musics and music control

--- Assets/Scripts/MenuControl/SoundControl.cs
+++ Assets/Scripts/MenuControl/SoundControl.cs
@@ -1,13 +1,37 @@
 using FishNet.Managing.Timing;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.SceneManagement;
 
 public class SoundControl : MonoBehaviour
 {
     [SerializeField] private AudioSource[] soundSources;
     [SerializeField] private AudioClip menuMove, menuSelect, gunShoot;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioClip[] musicClips;
+    private List<int> usedSongs = new List<int>();
 
+    private void Update()
+    {
+        // Don't run this code in Main Menu
+        if (SceneManager.GetActiveScene().name == "MainMenu") return;
+
+        if(!musicSource.isPlaying)
+        {
+            // Music source stopped playing audio, loop through the not used BG musics and play one
+            int number;
+            do
+            {
+                number = Random.Range(0, 8);
+            } while (usedSongs.Contains(number));
+            
+            usedSongs.Add(number);
+            PlayNextBGMusic(musicClips[number]);
+        }
+    }
 
     public void PlaySound(AudioClip soundEffect, float volume)
     {
@@ -40,5 +64,11 @@ public class SoundControl : MonoBehaviour
     {
         float volume = 0.4f;
         PlaySound(gunShoot, volume);
+    }
+
+    public void PlayNextBGMusic(AudioClip musicToPlay)
+    {
+        musicSource.clip = musicToPlay;
+        musicSource.Play();
     }
 }
