4924fe417c828676a209cdda8ff4fa01e48911ba
Haltimo Samuli <samuli.haltimo@metropolia.fi>
2023-05-01 12:52:41 +0300

    kill feed colored

--- Assets/Scripts/UI/KillFeed.cs
+++ Assets/Scripts/UI/KillFeed.cs
@@ -7,6 +7,9 @@ public class KillFeed : NetworkBehaviour
     [SerializeField] private GameObject _entryTemplate;
     [SerializeField] private Transform _feedContainer;
 
+    [SerializeField] private Color redColor = new Color(255, 0, 2, 255);
+    [SerializeField] private Color greenColor = new Color(0, 255, 11, 255);
+
     PlayerManager _playerManager;
 
     public override void OnStartClient()
@@ -23,12 +26,22 @@ public class KillFeed : NetworkBehaviour
     }
 
     [ObserversRpc]
-    private void CreateKillFeed(string killerName, string victimName)
+    private void CreateKillFeed(string killerName, string victimName, int killerTeamTag)
     {
         Transform entryTransform = Instantiate(_entryTemplate, _feedContainer).transform;
 
         entryTransform.Find("KillerName").GetComponent<TMPro.TextMeshProUGUI>().text = killerName;
         entryTransform.Find("VictimName").GetComponent<TMPro.TextMeshProUGUI>().text = victimName;
+        if(killerTeamTag == 0)
+        {
+            entryTransform.Find("KillerName").GetComponent<TMPro.TextMeshProUGUI>().color = redColor;
+            entryTransform.Find("VictimName").GetComponent<TMPro.TextMeshProUGUI>().color = greenColor;
+        }
+        else
+        {
+            entryTransform.Find("KillerName").GetComponent<TMPro.TextMeshProUGUI>().color = greenColor;
+            entryTransform.Find("VictimName").GetComponent<TMPro.TextMeshProUGUI>().color = redColor;
+        }
 
         Destroy(entryTransform.gameObject, 5);
     }
