91f5c27ceb22b293a4c180aa0dd2b19ecc771a44
Haltimo Samuli <samuli.haltimo@metropolia.fi>
2023-05-02 11:22:57 +0300

    player hit sound

--- Assets/Scripts/MenuControl/SoundControl.cs
+++ Assets/Scripts/MenuControl/SoundControl.cs
@@ -96,7 +96,7 @@ public class SoundControl : MonoBehaviour
 
     public void PlayPlayerHit()
     {
-        float volume = globalVolume * 2;
+        float volume = globalVolume * 4;
         PlaySound(playerHitSound, volume, globalPitch, 0.25f);
     }
 
