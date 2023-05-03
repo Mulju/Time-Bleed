6c1babeba85d75b6072270baf9bad20ac14b0a38
Tino Behnen <95407806+p0p4@users.noreply.github.com>
2023-05-03 16:30:21 +0300

    sound levels for pickup

--- Assets/Scripts/MenuControl/SoundControl.cs
+++ Assets/Scripts/MenuControl/SoundControl.cs
@@ -131,13 +131,13 @@ public class SoundControl : MonoBehaviour
 
     public void PlayChronadePickup()
     {
-        float volume = 0.5f;
+        float volume = 0.25f;
         PlaySound(chronoPickuUpSound, volume, globalPitch);
     }
 
     public void PlayHealthPickup()
     {
-        float volume = 1f;
+        float volume = 1.5f;
         PlaySound(healthPickUpSound, volume, globalPitch);
     }
 
